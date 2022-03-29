42262e5eb4ae420f477d44907dfb0b962c10aaa4
Conordoherty165 <75123069+Conordoherty165@users.noreply.github.com>
2022-03-29 04:45:28 +0100

    Changes made by making Adaptive Stego

--- StegoedGifImage.java
+++ StegoedGifImage.java
@@ -119,7 +119,7 @@ public class StegoedGifImage {
 		// calls a method to save the stegoed image sending the method the 3D array, the
 		// height and width
 		ImageBuilder i = new ImageBuilder(stegoedImage, MAXROWS, MAXCOLS, gifFileStoreName); // creates a SaveImage
-																								// object,
+		// object,
 	}
 
 	// Hides the binary bits in the least significant bit of the blue color
@@ -705,8 +705,8 @@ public class StegoedGifImage {
 		return imagePixels;
 	}
 
-	public int[][][] stegoTheImageAdaptive(int[][][] imagePixels, int MAXROWS, int MAXCOLS, String[] binaryString,
-			int[] dataPositions) {
+	public  int[][][] stegoTheImageAdaptive(int[][][] imagePixels, int MAXROWS, int MAXCOLS, String[] binaryString, int[]dataPositions)
+	{
 		int[][] imageValuesTable = new int[8][256];
 		int tempRed = 256;
 		int tempGreen = 256;
@@ -729,7 +729,7 @@ public class StegoedGifImage {
 		int tempROWS = 0;
 		int tempCOLS = 0;
 
-		// temporary variables for testing
+		//temporary variables for testing
 		int borderunsuitability = 0;
 		int embedstage1 = 0;
 		int embedstage2 = 0;
@@ -739,6 +739,371 @@ public class StegoedGifImage {
 		int unsuitable = 0;
 		int unsuitablestage1 = 0;
 
+		//Create an array of all unique colours in the image 
+		imageValuesTable[0][coloursFound] = imagePixels[0][0][0];
+		imageValuesTable[1][coloursFound] = imagePixels[0][0][1];
+		imageValuesTable[2][coloursFound] = imagePixels[0][0][2];
+		imageValuesTable[3][coloursFound] = (imagePixels[0][0][0] + imagePixels[0][0][1] + imagePixels[0][0][2]) % 2;
+
+		//1.  Above code section Initialises the values and creates an Array of all unique colours, imageValueTable[3] takes the values of 0,1,2 and 
+		//adding them together and then checking if it is an even value.
+		if (coloursFound < 255) {
+			coloursFound ++;
+			}
+
+		for(row=0; row<MAXROWS; row++){
+			for(col=0; col<MAXCOLS; col++) 
+			{
+
+				foundFlag = 'n';
+
+				for(counter=0; counter<=coloursFound; counter++)
+				{
+					if (imagePixels[row][col][0] == imageValuesTable[0][counter]&&imagePixels[row][col][1] == imageValuesTable[1][counter]&&imagePixels[row][col][2] == imageValuesTable[2][counter])
+					{
+						foundFlag = 'y';
+						break;
+					}
+				}
+
+				if (foundFlag == 'n')
+
+				{
+					imageValuesTable[0][coloursFound] = imagePixels[row][col][0];
+					imageValuesTable[1][coloursFound] = imagePixels[row][col][1];
+					imageValuesTable[2][coloursFound] = imagePixels[row][col][2];
+					imageValuesTable[3][coloursFound] = (imagePixels[row][col][0] + imagePixels[row][col][1] + imagePixels[row][col][2]) % 2;
+					if (coloursFound < 255)
+						coloursFound ++;
+				}  
+			}
+		}
+		//2.       
+		//creating map of all the colours in the image and checking if the found flag is found and then assigning it "y" for yes and then breaking out of the for loop, 
+		//if the found flag value is still equal to "n" increment the coloursFound value by 1.
+
+		for(testParity =0; testParity <=coloursFound; testParity++)
+		{
+			if (imageValuesTable[3][testParity] == 1)
+				parity1Flag = 'Y';
+			if (imageValuesTable[3][testParity] == 0)
+				parity0Flag = 'Y';
+		}     
+
+		if (parity1Flag == 'N' || parity0Flag == 'N')
+		{
+			JOptionPane.showMessageDialog(null, "This image is not suitable for stegoing with this method as all of its pixels have the same parity.\nYour stegoed image will be displayed but the message will not be retrievable. ", 
+					"Message", JOptionPane.ERROR_MESSAGE);
+			return imagePixels;
+		}           
+
+		//3.
+		//step 3 check the image is suitable for stegoing
+		//if every parity value is the same the image is not suitable for this method
+		//if the loop didnt run display error message saying that the image is not suitable for stegoing
+
+
+		for(tablePosition =0; tablePosition <=coloursFound; tablePosition++)
+		{
+			tempRed = 256;
+			tempGreen = 256;
+			tempBlue = 256;
+			tempDistance = 3000;
+			distance = 3000;
+			tempParity = 4321;
+			currentParity = 54321;
+
+			for(tableRow=0; tableRow<=coloursFound; tableRow++)  
+			{
+				//4. 
+				//step 4 check the imageValuesTable to match each colour with the closest colour by distance which has a different parity   
+				if (tablePosition != tableRow)
+				{
+					distance = Math.sqrt( Math.pow((imageValuesTable[0][tablePosition] - imageValuesTable[0][tableRow]),2) + Math.pow((imageValuesTable[1][tablePosition] - imageValuesTable[1][tableRow]),2) + Math.pow((imageValuesTable[2][tablePosition] - imageValuesTable[2][tableRow]),2));            
+
+					//5. the above code is doing the distance formula for the distance between the two pixel values
+
+					currentParity = ((imageValuesTable[0][tableRow] + imageValuesTable[1][tableRow] + imageValuesTable[2][tableRow]) % 2);
+
+					if (distance < tempDistance) 
+					{                     
+						if(currentParity!=imageValuesTable[3][tablePosition])
+						{                                       
+							tempRed = imageValuesTable[0][tableRow];
+							tempGreen = imageValuesTable[1][tableRow];
+							tempBlue = imageValuesTable[2][tableRow];
+							tempDistance = distance;
+							tempParity = currentParity;
+						} 
+					}
+				}
+			}
+			//6. current Parity is
+			// temp distance is previous distance from the last loop, and the code checks if the value is less than the previous value
+			// if the distance is less than the previous distance check if the current parity is not equal to imageValueTable[0,1,2] % 2 
+			//and then if it is not set the tempColour values to mageValueTable[0,1,2], as well as that change the tempDistance to the distance calculated above and
+			//change the tempParity which was equal to 4321 to the currentParity that was worked out on line 826.
+
+			imageValuesTable[4][tablePosition]=tempRed;
+			imageValuesTable[5][tablePosition]=tempGreen;
+			imageValuesTable[6][tablePosition]=tempBlue;
+			imageValuesTable[7][tablePosition]= tempParity; 
+		}
+
+		//7.      
+		//step 7 cycle through the image and the binaryString.
+		//for each pixel whose parity does not equal the corresponing binary value replace it with the closest colour which has a different parity as found in the imageValuesTable
+
+		i = 0;
+		tempROWS = 0;
+		tempCOLS = 0;     
+
+		System.out.print("binaryString.length = ");
+		System.out.println(binaryString.length);
+
+
+
+		counter = 0;
+
+		do
+		{
+			counter++;
+			int temp = dataPositions[counter];
+
+			if (dataPositions[counter] <= (MAXCOLS / 3))
+			{
+				if (dataPositions[counter] >= 1) {
+					tempCOLS = (dataPositions[counter] - 1);
+				}
+			}
+			else
+			{     
+				while (dataPositions[counter] > (MAXCOLS / 3))
+				{
+					dataPositions[counter] = dataPositions[counter] - (MAXCOLS / 3);
+					tempROWS++;
+
+					if (dataPositions[counter] <= (MAXCOLS / 3))
+					{
+						tempCOLS = (dataPositions[counter] - 1);
+						break;
+					}
+				}
+			}
+			//8. loop increments counter by 1 when in loop, checks if the dataposition[counter] is less than a third of the MAXCOL Length 
+			//  in the else statement it checks where the data position is and then the dataposition counter value is taken away from the first 3rd of the MAXCOL's
+			
+
+			tempROWS = ((tempROWS * 3) - 1); //array index?
+			tempCOLS = ((tempCOLS * 3) - 1);
+
+			if ((tempROWS > 200) || (tempCOLS > 300))
+			{
+				System.out.println("coords check");
+				System.out.println(tempROWS);
+				System.out.println(tempCOLS);
+
+				//9. tempROWS and tempCOLS are checked if they are under 200 for ROWS and 300 for COLS
+				//then prints out a coordinate check if the statements are met
+				System.out.println(dataPositions[counter]);
+				System.out.println(temp); //last Data Position Counter
+			}
+
+			if ((tempROWS == -1) || (tempCOLS == -1))
+				borderunsuitability++; //test flag
+
+			if ((tempROWS != - 1)&&(tempCOLS != - 1))
+			{
+				if (binaryString[i+1] == null)   
+				{
+					System.out.print("Border values out of range = ");
+					System.out.println(borderunsuitability);
+
+					System.out.print("Suitable after first check= ");
+					System.out.println(embedstage1);
+
+					System.out.print("Unsuitable after first check= ");
+					System.out.println(unsuitablestage1);
+
+					System.out.print("Unsuitable after embedding and rechecking= ");
+					System.out.println(embedstage2unsuitable);
+
+					System.out.print("Embedded in image= ");
+					System.out.println(embedstage2);
+
+					return imagePixels;     //returns the stegoed 3D array
+				}
+
+
+				char suitabilityFlag = 'y';
+				unsuitable = 0;
+
+				if ((tempROWS >= MAXROWS-1) || (tempCOLS >= MAXCOLS-1))
+				{ 
+					suitabilityFlag = 'n';
+					borderunsuitability++; //test flag
+					System.out.println("BP");
+				}
+				//10.you don’t need to explain above. It is checking pixels on the edges of the image.
+				else
+				{
+					if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS-1][tempCOLS-1][0]) 
+							|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS-1][tempCOLS-1][1])
+							|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS-1][tempCOLS-1][2]))
+						different++;
+					else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS-1][tempCOLS][0]) 
+							|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS-1][tempCOLS][1])
+							|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS-1][tempCOLS][2]))
+						different++;
+					else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS-1][tempCOLS+1][0]) 
+							|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS-1][tempCOLS+1][1])
+							|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS-1][tempCOLS+1][2]))
+						different++;
+					else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS][tempCOLS+1][0]) 
+							|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS][tempCOLS+1][1])
+							|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS][tempCOLS+1][2]))
+						different++;
+					else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS+1][tempCOLS+1][0]) 
+							|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS+1][tempCOLS+1][1])
+							|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS+1][tempCOLS+1][2]))
+						different++;
+					else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS+1][tempCOLS][0]) 
+							|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS+1][tempCOLS][1])
+							|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS+1][tempCOLS][2]))
+						different++;
+					else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS+1][tempCOLS-1][0]) 
+							|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS+1][tempCOLS-1][1])
+							|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS+1][tempCOLS-1][2]))
+						different++;
+					else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS][tempCOLS-1][0]) 
+							|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS][tempCOLS-1][1])
+							|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS][tempCOLS-1][2]))
+						different++;   
+				}
+
+				//11.         Checking pixels around if the surround pixels are different to current pixels.
+				if (different == 0)
+					unsuitablestage1 ++;
+
+
+				if (suitabilityFlag == 'y' && different > 0)    
+				{              
+					embedstage1++;
+
+					char binaryValue = binaryString[i].charAt(0);      
+					if (binaryValue == '1')                      
+					{
+						tempParity = ((imagePixels[tempROWS][tempCOLS][0] + imagePixels[tempROWS][tempCOLS][1] + imagePixels[tempROWS][tempCOLS][2]) % 2);
+						if (tempParity == 0)
+						{
+							for(int counter2 = 0; counter2 <= coloursFound; counter2++)
+							{
+								if (imagePixels[tempROWS][tempCOLS][0] == imageValuesTable[0][counter2]&&imagePixels[tempROWS][tempCOLS][1] == imageValuesTable[1][counter2]&&imagePixels[tempROWS][tempCOLS][2] == imageValuesTable[2][counter2])
+								{
+									imagePixels[tempROWS][tempCOLS][0] = imageValuesTable[4][counter2];
+									imagePixels[tempROWS][tempCOLS][1] = imageValuesTable[5][counter2];
+									imagePixels[tempROWS][tempCOLS][2] = imageValuesTable[6][counter2];
+									break;
+								}
+							}
+						}
+					}   
+					else
+						//12.      this code shows that if they are not diffenerent it is not sutiable as the image will show clearly that it has been altered by stegoing                                          {
+						tempParity = ((imagePixels[tempROWS][tempCOLS][0] + imagePixels[tempROWS][tempCOLS][1] + imagePixels[tempROWS][tempCOLS][2]) % 2);
+					if (tempParity == 1)
+					{
+						for(int counter3 = 0; counter3 <= coloursFound; counter3++)
+						{
+							if (imagePixels[tempROWS][tempCOLS][0] == imageValuesTable[0][counter3]&&imagePixels[tempROWS][tempCOLS][1] == imageValuesTable[1][counter3]&&imagePixels[tempROWS][tempCOLS][2] == imageValuesTable[2][counter3])
+							{
+								imagePixels[tempROWS][tempCOLS][0] = imageValuesTable[4][counter3];
+								imagePixels[tempROWS][tempCOLS][1] = imageValuesTable[5][counter3];
+								imagePixels[tempROWS][tempCOLS][2] = imageValuesTable[6][counter3];
+								break;
+							}
+						}
+					}  
+				}     
+				//13. 
+				// creates tempParity the same as done above and then checks if the tempParity is equal to 1
+				//and if it is equal to 1 do a for loop which declares and initialises counter 3 and increments by 1
+				//compares the temps against the current values, see if it is the same, these values are going to be used in the if statement below.
+				//if it was suitable above and is still suitable after embedding i++
+				different2=0;
+
+				if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS-1][tempCOLS-1][0]) 
+						|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS-1][tempCOLS-1][1])
+						|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS-1][tempCOLS-1][2]))
+					different2++;
+				else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS-1][tempCOLS][0]) 
+						|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS-1][tempCOLS][1])
+						|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS-1][tempCOLS][2]))
+					different2++;
+				else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS-1][tempCOLS+1][0]) 
+						|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS-1][tempCOLS+1][1])
+						|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS-1][tempCOLS+1][2]))
+					different2++;
+				else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS][tempCOLS+1][0]) 
+						|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS][tempCOLS+1][1])
+						|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS][tempCOLS+1][2]))
+					different2++;
+				else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS+1][tempCOLS+1][0]) 
+						|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS+1][tempCOLS+1][1])
+						|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS+1][tempCOLS+1][2]))
+					different2++;
+				else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS+1][tempCOLS][0]) 
+						|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS+1][tempCOLS][1])
+						|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS+1][tempCOLS][2]))
+					different2++;
+				else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS+1][tempCOLS-1][0]) 
+						|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS+1][tempCOLS-1][1])
+						|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS+1][tempCOLS-1][2]))
+					different2++;
+				else if ((imagePixels[tempROWS][tempCOLS][0] != imagePixels[tempROWS][tempCOLS-1][0]) 
+						|| (imagePixels[tempROWS][tempCOLS][1] != imagePixels[tempROWS][tempCOLS-1][1])
+						|| (imagePixels[tempROWS][tempCOLS][2] != imagePixels[tempROWS][tempCOLS-1][2]))
+					different2++;           
+
+
+
+				if (different2 > 0)
+				{           
+					i++;
+					embedstage2++; 
+				}
+				else
+				{
+					embedstage2unsuitable++;
+				}
+				different2 = 0;
+
+			}           
+			different = 0;
+
+
+			tempCOLS = 0;
+			tempROWS = 0;
+
+		} while (i < binaryString.length);
+
+
+		System.out.print("Border values out of range = ");
+		System.out.println(borderunsuitability);
+
+		System.out.print("Suitable after first check= ");
+		System.out.println(embedstage1);
+
+		System.out.print("Unsuitable after first check= ");
+		System.out.println(unsuitablestage1);
+
+		System.out.print("Unsuitable after embedding and rechecking= ");
+		System.out.println(embedstage2unsuitable);
+
+		System.out.print("Embedded in image= ");
+		System.out.println(embedstage2);
+
 		return imagePixels;
 	}
 }
+
